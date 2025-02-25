71bc04aeae2d7f30e69c68c7f4530cff2cf4c523
crathgeb <chrisrathgeb@gmail.com>
2025-02-25 10:33:46 -0500

    chore: initial commit

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,22 @@
+import { useTheme } from "@/components/theme-provider"
+import { Switch } from "@radix-ui/react-switch"
+import { useState } from "react"
+export function ModeToggle() {
+
+    const [isChecked, setIsChecked] = useState<boolean>(false)
+  const { setTheme } = useTheme()
+
+  const changeTheme = (checked: boolean) => {
+    setIsChecked(checked)   
+    setTheme(checked ? "dark" : "light")
+  }
+
+  return (
+    <Switch
+        checked={isChecked}
+        onCheckedChange={changeTheme}
+        disabled
+        aria-readonly
+    />
+  )
+}
